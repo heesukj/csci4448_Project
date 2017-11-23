769dba638e7453354a3b81d6a229e83881810987
David-Zhuzhunashvili <david.zhuzhunashvili@colorado.edu>
2017-11-23 11:45:18 -0700

    initial spring boot application

---
+++ SpringBoot/src/main/java/com/stockscore/stockscore/StockscoreApplication.java
@@ -0,0 +1,12 @@
+package com.stockscore.stockscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockscoreApplication.class, args);
+	}
+}
