952b48ff90a3fec9b1a746b4e10b012a16113e00
David-Zhuzhunashvili <david.zhuzhunashvili@colorado.edu>
2017-12-09 15:07:45 -0700

    fixed stockpage and alert

---
+++ stockscore_new/src/main/java/com/stockscore/stockscore_new/data/nonEntity/Alert.java
@@ -0,0 +1,13 @@
+package com.stockscore.stockscore_new.data.nonEntity;
+
+public class Alert {
+    private String currentState;
+
+    public void setCurrentState(String currentState) {
+        this.currentState = currentState;
+    }
+
+    public String getCurrentState() {
+        return currentState;
+    }
+}
